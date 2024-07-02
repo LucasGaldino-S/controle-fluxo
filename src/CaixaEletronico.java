99019f812abdd25c6f1fd6e4bd3a826857087753
LucasGaldino-S <galdinosaraiva@gmail.com>
2024-07-02 20:06:26 -0300

    condicional composta

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,5 +1,5 @@
 public class CaixaEletronico {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
     
         double saldo = 25.0;
         double valorSolicitado = 17.0;
